778ca627b438a6a1317d1db98930e97ef3ec8b22
CocoOreo <songhaoranuestc@163.com>
2022-03-05 16:38:22 -0500

    fix: Fix the receiver buffer problem.

--- receiver.py
+++ receiver.py
@@ -18,6 +18,15 @@ def receive(file, emulatorAddr, emuReceiveACK, client_udp_sock):
         global packet_buffer
         global hasBufferedEOT
         global EOTSeqnum
+
+        # If we have received the EOT Packet and has already have all data packets, send EOT back
+        if hasBufferedEOT and (expected_pkt_num == EOTSeqnum):
+            EOT = Packet(2, seq_num, 0, '')
+            print("Send EOT Packet")
+            client_udp_sock.sendto(EOT.encode(), (emulatorAddr, emuReceiveACK))
+            done = True
+            break
+
         msg, _ = client_udp_sock.recvfrom(4096)
         data_packet = Packet(msg)
         packet_type = data_packet.typ
@@ -34,29 +43,28 @@ def receive(file, emulatorAddr, emuReceiveACK, client_udp_sock):
         if packet_type == 2:
             hasBufferedEOT = True
             EOTSeqnum = seq_num
-            print("BUffer EOT" , hasBufferedEOT, "SEQ", EOTSeqnum)
-        # If we have received the EOT Packet and has already have all data packets, send EOT back
-        if hasBufferedEOT and (expected_pkt_num == EOTSeqnum):
-            EOT = Packet(2, seq_num, 0, '')
-            print("Send EOT Packet")
-            client_udp_sock.sendto(EOT.encode(), (emulatorAddr, emuReceiveACK))
-            done = True
-            break
+            print("BUffer EOT", hasBufferedEOT, "SEQ",
+                  EOTSeqnum, "expect", expected_pkt_num)
 
         # receives expected data packet
         if packet_type == 1:
             client_udp_sock.sendto(
                 Packet(0, seq_num, 0, '').encode(), (emulatorAddr, emuReceiveACK))
             # Buffer packet
-            if (seq_num >= expected_pkt_num and seq_num <= expected_pkt_num + 9) or (expected_pkt_num > 22 and seq_num <= ((expected_pkt_num + 9) % SEQ_MODULO)):
-                packet_buffer[seq_num] = data
+            if expected_pkt_num >= 23:
+                if (expected_pkt_num <= seq_num and seq_num <= 31) or seq_num <= ((expected_pkt_num + 9) % SEQ_MODULO):
+                    packet_buffer[seq_num] = data
+            else:
+                if seq_num >= expected_pkt_num and seq_num <= expected_pkt_num + 9:
+                    packet_buffer[seq_num] = data
+            # if (seq_num >= expected_pkt_num and seq_num <= expected_pkt_num + 9) or (expected_pkt_num > 22 and seq_num <= ((expected_pkt_num + 9) % SEQ_MODULO)):
+            #     packet_buffer[seq_num] = data
 
             while expected_pkt_num in packet_buffer:
                 file.write(packet_buffer[expected_pkt_num].encode())
                 del packet_buffer[expected_pkt_num]
                 expected_pkt_num = (expected_pkt_num + 1) % SEQ_MODULO
                 print("Final Exp", expected_pkt_num, "EOT NUM", EOTSeqnum)
-
 
 def writeLogFile():
     # Writing log file
